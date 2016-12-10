f4c0f6878949b280592b90b8cf440c396816fb7e
John Howard <fatred@gmail.com>
2016-12-10 21:44:43 +0000

    added template and default node importer

--- ltm/__init__.py
+++ ltm/__init__.py
@@ -13,9 +13,23 @@ with open("ltm.conf", 'r') as stream:
         print(exc)
 
 
+def load_defaults(**kwargs):
+    if kwargs['objtype'] == 'Node':
+        pass
+    elif kwargs['objtype'] == 'Pool':
+        pass
+    elif kwargs['objtype'] == 'VirtualServer':
+        pass
+    return dict
+
+
 class Node(object):
     def __init__(self, **kwargs):
+        # first init the node with defaults
+        defaults = load_defaults(objtype='Node')
+        for (k, v) in defaults.items():
+            setattr(self, k, v)
         # populate the Node params with things we passed in
+        for (k, v) in kwargs:
+            setattr(self, k, v)
 
-        for key in kwargs:
-            self[key] = kwargs[key]
