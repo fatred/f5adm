84999cad5cbb7902c78d20f6c3c97fafb34d13d4
John Howard <fatred@gmail.com>
2016-12-10 22:15:00 +0000

    Swapped the objects to dict inheritance

--- ltm/__init__.py
+++ ltm/__init__.py
@@ -1,12 +1,15 @@
 import yaml
+import os
 
-with open('template.yaml', 'r') as stream:
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'template.yaml'), 'r') as stream:
     try:
         template = yaml.load(stream)
     except yaml.YAMLError as exc:
         print(exc)
 
-with open("ltm.conf", 'r') as stream:
+with open(os.path.join(__location__, "ltm.yaml"), 'r') as stream:
     try:
         ltmconfig = yaml.load(stream)
     except yaml.YAMLError as exc:
@@ -25,23 +28,31 @@ def load_defaults(**kwargs):
     return defaults
 
 
-class Node(object):
+class Node(dict):
     def __init__(self, **kwargs):
-        # first init the node with defaults
+        # super the dict stuff in
+        super(Node, self).__init__(**kwargs)
+
+        # first init the node with yaml defaults
         defaults = load_defaults(objtype='Node')
         for (k, v) in defaults.items():
             setattr(self, k, v)
-        # populate the Node params with things we passed in
+
+        # populate the Node params with things we passed in on the object creation
         for (k, v) in kwargs:
             setattr(self, k, v)
 
 
 class Pool(object):
     def __init__(self, **kwargs):
+        # super the dict stuff in
+        super(Pool, self).__init__(**kwargs)
+
         # first init the pool with defaults
         defaults = load_defaults(objtype='Pool')
         for (k, v) in defaults.items():
             setattr(self, k, v)
+
         # populate the pool params with things we passed in
         for (k, v) in kwargs:
             setattr(self, k, v)
@@ -49,10 +60,14 @@ class Pool(object):
 
 class VirtualServer(object):
     def __init__(self, **kwargs):
+        # super the dict stuff in
+        super(VirtualServer, self).__init__(**kwargs)
+
         # first init the VIP with defaults
         defaults = load_defaults(objtype='VirtualServer')
         for (k, v) in defaults.items():
             setattr(self, k, v)
+
         # populate the VIP params with things we passed in
         for (k, v) in kwargs:
             setattr(self, k, v)
